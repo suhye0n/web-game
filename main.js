4ee639cff0a84eb4d88c6f0d4a8d33feacb354ba
suhye0n <contact.suhyeon@gmail.com>
2022-12-02 00:36:21 +0900

    애니메이션 추가

--- main.js
+++ main.js
@@ -11,6 +11,47 @@ var dino = {
     height: 50,
     draw() {
         ctx.fillStyle = 'green';
-        ctx.fillRect(10, 10, 100, 100);
+        ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 }
+dino.x += 1;
+dino.draw()
+
+class Cactus {
+    constructor() {
+        this.x = 500;
+        this.y = 200;
+        this.width = 50;
+        this.height = 50;
+    }
+    draw() {
+        ctx.fillStyle = 'red';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+var cactus = new Cactus();
+cactus.draw();
+
+var timer = 0;
+var cacti = [];
+
+function run() {
+    requestAnimationFrame(run);
+    timer++;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    if (timer % 120 === 0) {
+        var cactus = new Cactus();
+        cacti.push(cactus);
+    }
+
+    cacti.forEach((a)=>{
+        a.x--;
+        a.draw();
+    })
+
+    dino.draw();
+}
+
+run();
